9fa02638d1b97a275971f06227986f5c7e3b3444
valentinsabbath <valentingavela@gmail.com>
2018-05-24 17:21:40 +0000

    commit.sh

--- install_modules.sh
+++ install_modules.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cpan HTML::Template
-cpan JSON
-apt install unclutter
+apt update &&
+cpan HTML::Template &&
+cpan JSON &&
+apt install unclutter &&
+cp config_autostart/etc_xdg_lxsession_LXDE_autostart /etc/xdg/lxsession/LXDE/autostart &&
+mkfifo /var/www/omxplayerpipe &&
+chown www-data:www:data /var/www/omxplayerpipe
+usermod -a -G video www-data &&
+usermod -a -G audio www-data &&
+cp config_autostart/boot_cmdline.txt /boot/cmdline.txt
